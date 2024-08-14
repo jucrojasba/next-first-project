f38b7c82641a82de7445802f378aef16324f3d21
Juan Camilo Rojas <jucrojasba@gmail.com>
2024-08-14 07:05:14 -0500

    Add internacionalization

--- app/link-1/page.tsx
+++ app/link-1/page.tsx
@@ -1,14 +1,23 @@
+"use client"
 // Importación de React si es necesario
 import React from 'react';
 import Link from 'next/link';
+import { useLanguage } from '@/global-states/languaje';
 
 const SecondPage = () => {
+  const language=useLanguage((state)=>state.language);
+  const {setLanguage}=useLanguage();
   return (
     <div>
-      <h1>Hello from my Second Page</h1>
+      <h1>{language==='english' ? 'Hello from my second page' : language==='spanish' ? 'Hola desde mi segunda página':''}</h1>
       <Link href="/">
-        Click here to go to the first page
+      {language==='english' ? 'Click here to go to the first page' : language==='spanish' ? 'Click aquí para ir a la primera página':''}
       </Link>
+      <label htmlFor="language-select">{language==='english' ? 'Select a language' : language==='spanish' ? 'Seleccione un lenguaje':''}</label>
+      <select id="language-select" onChange={(e) => setLanguage(e.target.value)}>
+          <option value="english">English</option>
+          <option value="spanish">Español</option>
+      </select>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
+"use client"
 import Link from 'next/link';
+import { useLanguage } from '@/global-states/languaje';
 
 export default function Home() {
+  const language=useLanguage((state)=>state.language);
+  const {setLanguage}=useLanguage();
   return (
     <main>
-      <h1>Hello from my first page</h1>
+      <h1>{language==='english' ? 'Hello from my first page' : language==='spanish' ? 'Hola desde mi primera página':''}</h1>
       <Link href="/link-1">
-        Click here to go to the second page
+      {language==='english' ? 'Click here to go to the second page' : language==='spanish' ? 'Click aquí para ir a la segunda página':''}
       </Link>
+      <label htmlFor="language-select">{language==='english' ? 'Select a language' : language==='spanish' ? 'Seleccione un lenguaje':''}</label>
+      <select id="language-select" onChange={(e) => setLanguage(e.target.value)}>
+          <option value="english">English</option>
+          <option value="spanish">Español</option>
+      </select>
     </main>
   );
 }
